1e703206b24c5e2fbdf697093d21395adfdefb66
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-05 18:14:05 -0300

    Adiciona funções para cadastrar pensamentos

--- js/api.js
+++ js/api.js
@@ -8,7 +8,26 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+    async SalvarPensamentos(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                }, 
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch{
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
     }
+
+
 }
 
 export default api;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 import ui from "./ui.js"
+import api from "./api.js"
+
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+
+    const botaoCancelar = document.getElementById("botao-cancelar")
+    botaoCancelar.addEventListener("click", limparFormulario)
 })
+
+function limparFormulario() {
+    document.getElementById("pensamento-id").value = ""
+    document.getElementById("pensamento-conteudo").value = ""
+    document.getElementById("pensamento-autoria").value = ""
+}
+
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.SalvarPensamentos({conteudo, autoria})
+        ui.renderizarPensamentos()
+    } catch {
+        alert("Erro ao salvar pensamento")
+    }
+}
